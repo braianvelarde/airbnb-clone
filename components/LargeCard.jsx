ab7e3ec495e7fec7098f7c4eb9b191ff2248613a
Braian Velarde <braianvelarde@gmail.com>
2021-09-08 05:28:53 -0300

    corregir estilos css

--- components/LargeCard.jsx
+++ components/LargeCard.jsx
@@ -12,7 +12,7 @@ function LargeCard({buttonText, img, title, subTitle}) {
             <div className="absolute top-32 left-12">
                 <h3 className="text-4xl mb-3 w-56 sm:w-64">{title}</h3>
                 <h4 className="text-2xl mb-3 w-64 sm:w-80">{subTitle}</h4>
-                <button className="bg-gray-900 text-white mt-5
+                <button className="bg-gray-900 text-white mt-5 
                 text-sm rounded-lg px-4 py-2 hover:bg-white hover:text-gray-900
                 transition duration-200">{buttonText}</button>
             </div>
